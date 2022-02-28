281ca3c98c83fca53f239598601a6a66163621c4
_Braga_ <brazhnik1602@mail.ru>
2022-02-28 05:07:47 +0300

    Role saeching added

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -5,6 +5,8 @@ import {Employee} from '../employee';
 import {TokenStorageService} from '../_services/token-storage.service';
 import { Table } from 'primeng/table';
 import { PrimeNGConfig } from 'primeng/api';
+import {of} from 'rxjs';
+import {callbackify} from 'util';
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
@@ -20,39 +22,47 @@ export class EmployeeListComponent implements OnInit {
   roleAdmin = 'admin';
   statuses: any[];
   loading: boolean = true;
-  employee: Employee;
-  qwe:boolean = false;
   private roles: string[];
   constructor(private employeeService: EmployeeService,
               private router: Router, private tokenStorageService: TokenStorageService, private primengConfig: PrimeNGConfig) { }
 
   ngOnInit(): void {
+
     this.employeeService.getEmployeesLarge().then(employees => {
       this.employees = employees;
       this.loading = false;
-      if ('ROLE_ADMIN' in this.employee.roles){
-      this.qwe = true;
-    }
     });
     this.statuses = [
       {label: 'ROLE_ADMIN', value: 'ROLE_ADMIN'},
       {label: 'ROLE_USER', value: 'ROLE_USER'},
       {label: 'ROLE_DOCTOR', value: 'ROLE_DOCTOR'}
     ];
+
     const user = this.tokenStorageService.getUser();
     this.roles = user.roles;
     this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+
     this.getEmployees();
+
+
   }
   private getEmployees(){
+
     this.employeeService.getEmployeesList().subscribe(data => {
+      data.forEach(function(value){
+        value.role = value.roles[0].name;});
       this.employees = data;
     });
+
   }
 
   employeeDetails(id: number){
     this.router.navigate(['employee-details', id]);
   }
+  checkrole(employee: Employee){
+      employee.role = employee.roles[0].name;
+    }
+
 
   updateEmployee(id: number){
     this.router.navigate(['update-employee', id]);
